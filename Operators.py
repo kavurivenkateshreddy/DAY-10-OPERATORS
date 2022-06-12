76cf8abf39638e285e1aca29a104e8136b479a8f
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-06-12 09:43:44 +0530

    Update Operators.py

--- Operators.py
+++ Operators.py
@@ -1,3 +1,8 @@
+"""
+Date: 21th MAY 2022 Monday
+Name: kavuri venkatesh reddy
+File Desc: OPERATORS
+"""
 """    Arithmetic Operators """
 # a=float(input("Enter the value of a:"))
 # b=float(input("Enter the value of b:"))
